b6e986a85ee5d69a3655f53e4d905e5b725f6101
Riazantsev <riazantsevmark327@gmail.com>
2023-08-12 20:42:51 +0400

    doest'n work

---
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+public class VacationServiceTest {
+
+        @ParameterizedTest
+        //@CsvSource({
+                //"3, 10000, 3000, 20000",
+              //  "2, 100000, 60000, 150000"
+
+       // })
+        @CsvFileSource (files = "src/test/resorses/vacation.csv")
+        public void shouldCalcExact(int count, int income, int expenses, int threshold) {
+            VacationService service = new VacationService();
+
+            //int expected = 5;
+            int actual = service.calculate (income, expenses, threshold);
+
+            Assertions.assertEquals(count, actual);
+        }
+    }
